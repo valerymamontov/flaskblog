a746073d5beaa84a4dab646d2913df93329ccb7e
Valery Mamontov <genius2003klg@mail.ru>
2019-06-19 08:41:55 +0300

    flask-security part-1

--- app/app.py
+++ app/app.py
@@ -5,6 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+from flask_security import SQLAlchemyUserDatastore
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -16,11 +20,7 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
 from models import *
-
-
 admin = Admin(app)
 admin.add_view(ModelView(Post, db.session))
 admin.add_view(ModelView(Tag, db.session))
--- app/models.py
+++ app/models.py
@@ -3,9 +3,18 @@ from datetime import datetime
 import re
 import random
 
+from flask_security import UserMixin, RoleMixin
+
+
+post_tag = db.Table(
+    'post_tag',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
 
 def func_slugify(s):
-    return s
+    return s.lower()
     # pattern = r'[^\w+]'
     # slug = re.sub(pattern, '-', s)
     # return slug
@@ -18,11 +27,6 @@ def func_slugify(s):
     #     str(random.randrange(1, 17))
     # )
 
-post_tag = db.Table(
-    'post_tag',
-    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
-)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -51,6 +55,7 @@ class Post(db.Model):
     def __repr__(self):
         return '{}'.format(self.title)
 
+
 class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(140))
@@ -62,3 +67,33 @@ class Tag(db.Model):
 
     def __repr__(self):
         return '{}'.format(self.name)
+
+
+# для flask-security
+
+roles_users = db.Table(
+    'roles_users',
+    db.Column('id_user', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('id_role', db.Integer(), db.ForeignKey('role.id'))
+    )
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255))
+    name = db.Column(db.String(140))
+    date_create = db.Column(db.DateTime, default=datetime.now())
+    active = db.Column(db.Boolean())
+
+    roles = db.relationship(
+        'Role',
+        secondary=roles_users,
+        backref=db.backref('users', lazy='dynamic')
+        )
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(255))
